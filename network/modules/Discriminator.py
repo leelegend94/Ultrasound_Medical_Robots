94259a62acccb7f51d109bff7c6392d0423de610
Zhenyu Li <zhenyu.li@tum.de>
2020-07-23 16:58:04 +0200

    debug a very noob bug

--- network/modules/Discriminator.py
+++ network/modules/Discriminator.py
@@ -6,36 +6,45 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         
-        self.img_conv1 = nn.Conv2d(1, 16, kernel_size = 3, padding=1)
-        self.img_conv2 = nn.Conv2d(16, 64, kernel_size = 3, padding=1)
-        self.img_pool = nn.MaxPool2d(2, 2)
-        
-        self.mask_conv = nn.Conv2d(1, 64, kernel_size = 3, padding=1)
-        self.mask_pool = nn.MaxPool2d(2, 2)
+        self.branch1 = Discriminator.image_branch()
+        self.branch2 = Discriminator.mask_branch()
         
         self.dis_conv1 = nn.Conv2d(128, 256, kernel_size = 3, padding=1)
         self.dis_pool1 = nn.MaxPool2d(2, 2)
         self.dis_conv2 = nn.Conv2d(256, 512, kernel_size = 3, padding=1)
-        self.dis_pool2 = nn.MaxPool2d(2, 2)
+        #self.dis_pool2 = nn.MaxPool2d(2, 2)
         
         self.output = nn.Linear(512,1)
         
         
     def forward(self, input, mask):
-        x = self.img_conv1(input)
-        x = self.img_conv2(x)
-        x = self.img_pool(x)
-        
-        y = self.mask_conv(mask)
-        y = self.mask_pool(y)
+        x = self.branch1(input)
+        y = self.branch2(mask)
         
         x = torch.cat([x,y],dim=1)
-        x = self.dis_conv1(x)
+        x = F.relu(self.dis_conv1(x))
         x = self.dis_pool1(x)
-        x = self.dis_conv2(x)
-        x = self.dis_pool2(x)
-        x = F.max_pool2d(x,kernel_size=x.size()[2:]).squeeze()
+        x = F.relu(self.dis_conv2(x))
+        #x = self.dis_pool2(x)
+        x = F.max_pool2d(x,kernel_size=x.size()[2:]).squeeze() #global max pooling
 
         output = torch.sigmoid(self.output(x))
-        
+
         return output
+
+    def image_branch():
+        return nn.Sequential(
+                nn.Conv2d(1, 16, kernel_size = 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(16, 64, kernel_size = 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2, 2)
+            )
+    def mask_branch():
+        return nn.Sequential(
+                nn.MaxPool2d(2, 2),
+                nn.Conv2d(1, 64, kernel_size = 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(2, 2)
+            )
